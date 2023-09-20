8207a8714eaccbec0e97953b69fd0039110ab3b7
Regelyn Guihama <115629578+RegelynGuihama@users.noreply.github.com>
2023-09-20 16:42:09 +0800

    implementing axios

--- lms_api/lms_api/settings.py
+++ lms_api/lms_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'django.contrib.admin',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -111,15 +113,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# REST_FRAMEWORK={
-#     'DEFAULT_AUTHENTICATION_CLASSES':[
-#         'rest_framework.authentication.TokenAuthentication',
-#     ],
-# }
+REST_FRAMEWORK={
+     'DEFAULT_AUTHENTICATION_CLASSES':[
+         'rest_framework.authentication.TokenAuthentication',
+     ],
+ }
 
-DEFAULT_AUTHENTICATION_CLASSES: (
-     'rest_framework.authentication.SessionAuthentication',
- )
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+}
+
+# DEFAULT_AUTHENTICATION_CLASSES: (
+#      'rest_framework.authentication.SessionAuthentication',
+#  )
 
 
 # Internationalization
@@ -143,3 +151,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS=True
+# CORS_ALLOWED_ORIGINS=[
+#     "http://localhost:3000",
+# ]
+
